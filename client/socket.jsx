1f6d2a9a0232f8ac2bd2e60b4b07ba6934add51f
tanishy7777 <tanishyelgoe777@gmail.com>
2025-07-01 03:41:55 +0530

    updated backedn to use railways

--- client/socket.jsx
+++ client/socket.jsx
@@ -1,8 +1,11 @@
 import { io } from 'socket.io-client';
 import { auth } from './src/firebase/config';
 
-export const socket = io('http://localhost:3000', { autoConnect: false });
-
+// export const socket = io('http://localhost:3000', { autoConnect: false });
+export const socket = io('https://jwbackend-production.up.railway.app', {
+  autoConnect: false,
+  transports: ['websocket', 'polling'],
+});
 export const updateSocketAuth = async () => {
   const user = auth.currentUser;
   if (user) {
